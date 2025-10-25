c0c233e1347b8609cc5561df9d09fa8780dda464
A-Brett2002 <alorenzom2@miumg.edu.gt>
2025-10-25 08:48:49 -0600

    committed

--- clientes.sql
+++ clientes.sql
@@ -12,3 +12,4 @@ CREATE TABLE `crud_express`.`cliente` (
     `pendiente` VARCHAR(45) NULL,
     PRIMARY KEY (`id`));
 INSERT INTO `crud_express`.`cliente` (`telefono`, `nombre`, `direccion`, `nit`, `precio`, `estado`, `fecha`, `pendiente`)
+VALUES ('123456789', 'Juan Perez', 'Calle Falsa 123', '1234567-8', '100.00', 'Activo', '2024-01-01', '350');
